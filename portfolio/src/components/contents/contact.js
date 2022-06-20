bda77b239ed6900613a4e3219cebeb2136946c74
kissy24 <kissy105808j@gmail.com>
2022-06-20 23:02:18 +0900

    I have listed a website for you to contact me.

--- portfolio/src/components/contents/contact.js
+++ portfolio/src/components/contents/contact.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 export default function Contact() {
     return (
-        <h1>Contact</h1>
+        <div>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <p><a href="https://github.com/kissy24">GitHub</a></p>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <p><a href="https://twitter.com/kissy_24">Twitter</a></p>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <p><a href="https://youtrust.jp/users/kissy24">Youtrust</a></p>
+                    </Grid>
+                </Grid>
+            </Box>
+        </div>
     );
 }
