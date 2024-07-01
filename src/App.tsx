69da9f3642e10feee24431813a882ce6074614b6
kissy24 <kissy105808j@gmail.com>
2024-07-02 00:22:23 +0900

    update page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import kissy24Image from '/kissy24.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={kissy24Image} className="image" alt="kissy24" />
+      <div className="divide-y">
+        <div className="font-bold text-xl mb-2">Kissy24</div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#プロダクトマネージャー</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#エンジニアリングマネージャー</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#ITエンジニア</span>
+        </div>
+        <div>
+          <a href="https://github.com/kissy24">GitHub </a>
+          <a href="https://twitter.com/kissy_24">X(Twitter) </a>
+          <a href="https://youtrust.jp/users/kissy24">Youtrust</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
+
 }
 
 export default App
