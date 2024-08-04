167c8a710f6bc105a73de40bff8dcec8cd985129
kissy24 <kissy105808j@gmail.com>
2024-08-04 18:11:45 +0900

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,35 @@ import githubImage from './img/github-mark.svg'
 
 function App() {
   return (
-    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
-      <div className="flex justify-center">
+    <div className="grid grid-cols-3 m-8 text-gray-600">
+      <div className="row-span-3 p-12">
         <img src={kissy24Image} className="rounded-full w-32 h-32 md:w-48 md:h-48 object-cover" alt="kissy24" />
+        <div className="text-3xl font-bold pt-3">Kissy24</div>
+        <div className="">Engineering Manager(DX Product Team), Software Developer(DX, ERP, NLP), Programming Language(Python, Go, Java)</div>
       </div>
-      <div className="mb-1 p-3 text-xl font-medium text-gray-900 dark:text-white">Kissy24</div>
-      <ul className="w-full my-4 space-y-3">
-        <li>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#PdM</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#EM</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Dev</span>
-        </li>
-        <li className="flex">
-          <a href="https://github.com/kissy24" className="flex items-center w-full p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-            <img src={githubImage} className="w-1/6" alt="github" />
-            <span className="w-5/6">GitHub</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/kissy_24" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">X(Twitter) </a>
-        </li>
-        <li>
-          <a href="https://youtrust.jp/users/kissy24" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">Youtrust</a>
-        </li>
-        <li>
-          <a href="https://qiita.com/kissy24" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">Qiita</a>
-        </li>
-        <li>
-          <a href="https://sizu.me/kissy24" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">しずかなインターネット</a>
-        </li>
-      </ul>
+      <div className="col-span-2 p-10">
+        <div className="text-2xl font-bold">Links</div>
+        <ul className="m-4 space-y-3">
+          <li>
+            <a href="https://github.com/kissy24" className="flex items-center w-full p-3 text-base font-bold rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow ">
+              <img src={githubImage} className="w-8" alt="github" />
+              <span className="w-8 ms-4">GitHub</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/kissy_24" className="flex items-center p-3 text-base font-bold rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow">X(Twitter) </a>
+          </li>
+          <li>
+            <a href="https://youtrust.jp/users/kissy24" className="flex items-center p-3 text-base font-bold rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow">Youtrust</a>
+          </li>
+          <li>
+            <a href="https://qiita.com/kissy24" className="flex items-center p-3 text-base font-bold rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow">Qiita</a>
+          </li>
+          <li>
+            <a href="https://sizu.me/kissy24" className="flex items-center p-3 text-base font-bold rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow">しずかなインターネット</a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 
